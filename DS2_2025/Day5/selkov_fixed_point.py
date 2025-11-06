0394162275b3b5ff6abd405c67f3eaf97eba1235
yugism <52194051+yugism@users.noreply.github.com>
2025-11-07 03:08:25 +0900

    Selkov Jacobina

--- DS2_2025/Day5/selkov_fixed_point.py
+++ DS2_2025/Day5/selkov_fixed_point.py
@@ -7,12 +7,8 @@ y = sp.Symbol('y')
 a = 0.06
 b = 0.6
 
-# ヌルクライン
-f = x / ( a + x**2 )
-g = b / ( a + x**2 )
+# 交点の座標を求める
+solutions = sp.solve( [    ,    ]  , ( x , y ) )
 
-# 交点の x 座標を求める。交点の x 座標は f = g となる x
-x_solutions = sp.solve( sp.Eq( f , g ) , x )
-
-print(x_solutions)
+print(solutions)
 
